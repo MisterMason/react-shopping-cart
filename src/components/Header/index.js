e41f350b5aca0c559b23b107f8ce28e87d46a634
MisterMason <masonsanders31@yahoo.com>
2021-03-22 02:30:01 -0400

    Initial Check-in

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style.scss';
+
+class Header extends React.Component {
+    render() {
+        return (
+             <div className="headerBox">
+                <h1 className="titleHeader">The Shoes Shopping Store</h1>
+             </div>
+        )
+    }
+}
+
+export default Header;
